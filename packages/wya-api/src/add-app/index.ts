d36b17540cee18574438dfd45d470eaee78d1928
Jan Navarro <navarrojanderrick@gmail.com>
2022-01-09 02:22:59 -0800

    Use dependency injection for fs

--- packages/wya-api/src/add-app/index.ts
+++ packages/wya-api/src/add-app/index.ts
@@ -5,25 +5,25 @@ import lintConfig from './templates/lint-config';
 import packageJson from './templates/package-json';
 import tsconfigJson from './templates/tsconfig-json';
 
-const addApp = (name: string) => {
+const addApp = (name: string, { fsInjection = fs } = {}) => {
   name = fp.kebabCase(name);
   console.log(`Adding [${name}] to apps workspace.`);
 
   const cwd = process.cwd();
   const dir = path.join(cwd, 'apps', name);
 
-  if (fs.existsSync(dir)) {
+  if (fsInjection.existsSync(dir)) {
     console.error(`[${name}] app already exists.`);
     return;
   }
 
-  fs.ensureDirSync(dir);
+  fsInjection.ensureDirSync(dir);
 
-  fs.writeFileSync(path.join(dir, '.eslintrc.js'), lintConfig());
-  fs.writeFileSync(path.join(dir, 'package.json'), packageJson(name));
-  fs.writeFileSync(path.join(dir, 'tsconfig.json'), tsconfigJson());
+  fsInjection.writeFileSync(path.join(dir, '.eslintrc.js'), lintConfig());
+  fsInjection.writeFileSync(path.join(dir, 'package.json'), packageJson(name));
+  fsInjection.writeFileSync(path.join(dir, 'tsconfig.json'), tsconfigJson());
 
-  fs.ensureDirSync(path.join(dir, 'src'));
+  fsInjection.ensureDirSync(path.join(dir, 'src'));
 
   console.log(`Added ${dir}`);
 };
--- packages/wya-api/src/add-package/index.ts
+++ packages/wya-api/src/add-package/index.ts
@@ -5,25 +5,25 @@ import lintConfig from './templates/lint-config';
 import packageJson from './templates/package-json';
 import tsconfigJson from './templates/tsconfig-json';
 
-const addPackage = (name: string) => {
+const addPackage = (name: string, { fsInjection = fs } = {}) => {
   name = fp.kebabCase(name);
   console.log(`Adding [${name}] to packages workspace.`);
 
   const cwd = process.cwd();
   const dir = path.join(cwd, 'packages', name);
 
-  if (fs.existsSync(dir)) {
+  if (fsInjection.existsSync(dir)) {
     console.error(`[${name}] package already exists.`);
     return;
   }
 
-  fs.ensureDirSync(dir);
+  fsInjection.ensureDirSync(dir);
 
-  fs.writeFileSync(path.join(dir, '.eslintrc.js'), lintConfig());
-  fs.writeFileSync(path.join(dir, 'package.json'), packageJson(name));
-  fs.writeFileSync(path.join(dir, 'tsconfig.json'), tsconfigJson());
+  fsInjection.writeFileSync(path.join(dir, '.eslintrc.js'), lintConfig());
+  fsInjection.writeFileSync(path.join(dir, 'package.json'), packageJson(name));
+  fsInjection.writeFileSync(path.join(dir, 'tsconfig.json'), tsconfigJson());
 
-  fs.ensureDirSync(path.join(dir, 'src'));
+  fsInjection.ensureDirSync(path.join(dir, 'src'));
 
   console.log(`Added ${dir}`);
 };
