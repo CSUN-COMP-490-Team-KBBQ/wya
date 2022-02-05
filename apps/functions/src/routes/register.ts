e96b76d85e1f9a63cd9e39b026291fd2d9767673
Jan Navarro <navarrojanderrick@gmail.com>
2022-02-05 14:05:34 -0800

    Refactor functions:api/register endpoint

--- apps/functions/src/routes/register.ts
+++ apps/functions/src/routes/register.ts
@@ -1,12 +1,39 @@
+import assert from 'assert';
 import { Router } from 'express';
-import createUser from '../middleware/createUser';
-import createUserRecord from '../middleware/createUserRecord';
+
+import {
+  etlFirebaseCreateNewUser,
+  etlFirebaseCreateNewUserRecord,
+} from 'wya-api';
 
 const router = Router();
 
-router.post('/', createUser, createUserRecord, (_req, res) => {
-    const uid = res.locals.uid;
-    res.status(200).send({ uid });
+router.post('/', async (req, res) => {
+  const logger = req.app.locals.functions.logger;
+  const { email, password, firstName, lastName } = req.body;
+  try {
+    // Create the user
+
+    const { data } = await etlFirebaseCreateNewUser({ email, password });
+    const uid = data?.uid;
+    assert(uid);
+
+    // Create the user record
+
+    await etlFirebaseCreateNewUserRecord({
+      uid,
+      email,
+      firstName,
+      lastName,
+    });
+
+    res.status(200).send({
+      data: { uid },
+    });
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json(err);
+  }
 });
 
 export default router;
