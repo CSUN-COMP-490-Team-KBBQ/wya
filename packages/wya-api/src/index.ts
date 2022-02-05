7bbef77b85f8c57a9bca12a0d5830318cd83a354
Jan Navarro <navarrojanderrick@gmail.com>
2022-02-05 10:08:21 -0800

    Refactor wya-api dir

--- packages/wya-api/src/index.ts
+++ packages/wya-api/src/index.ts
@@ -1,3 +1,3 @@
-export { default as addApp } from './add-app';
-export { default as addPackage } from './add-package';
-export { default as addReactComponent } from './add-react-component';
+// src exports
+
+export * from './modules';
---
+++ packages/wya-api/src/modules/cli/index.ts
@@ -0,0 +1,5 @@
+// cli exports
+
+export { default as addApp } from './add-app';
+export { default as addPackage } from './add-package';
+export { default as addReactComponent } from './add-react-component';
