0a24857f008c84c38da8aa891aff9753101b550c
Jan Navarro <navarrojanderrick@gmail.com>
2022-02-13 14:36:53 -0800

    Add delete-event-plan and delete-user-record to api firebase exports

--- packages/wya-api/src/modules/etl/firebase/index.ts
+++ packages/wya-api/src/modules/etl/firebase/index.ts
@@ -3,5 +3,9 @@
 export * from './create-new-event-plan';
 export * from './create-new-user';
 export * from './create-new-user-record';
+
+export * from './delete-event-plan';
 export * from './delete-user';
+export * from './delete-user-record';
+
 export * from './make-firebase-client';
