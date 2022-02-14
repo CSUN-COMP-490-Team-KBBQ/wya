dac460e07bc89819e99fd4f991281dd4b719714e
Jan Navarro <navarrojanderrick@gmail.com>
2022-02-13 16:03:54 -0800

    Refactor apps/functions

---
+++ apps/functions/src/routes/event-plans.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import {
+  etlFirebaseDeleteEventPlan,
+  etlFirebaseCreateNewEventPlan,
+} from 'wya-api';
+
+import { firebase } from '../firebase';
+
+const router = Router();
+
+router.post('/create', async (req, res) => {
+  const logger = req.app.locals.functions.logger;
+  const params = req.body;
+
+  try {
+    res
+      .status(200)
+      .json(await etlFirebaseCreateNewEventPlan(params, { firebase }));
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/', async (req, res) => {
+  const logger = req.app.locals.functions.logger;
+  const { eventPlanId } = req.body;
+
+  try {
+    await etlFirebaseDeleteEventPlan({ eventPlanId }, { firebase });
+    res.sendStatus(200);
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json(err);
+  }
+});
+
+export default router;
