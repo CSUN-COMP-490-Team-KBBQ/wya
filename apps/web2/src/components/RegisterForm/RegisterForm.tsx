d77b0228f19d2a0e6c0e07fe03ba28dfa58d3cbd
Jorge Escobar <jorge.escobar0419@gmail.com>
2022-04-02 01:36:57 -0700

    Add register page and register form to web2

--- apps/web2/src/components/RegisterForm/RegisterForm.tsx
+++ apps/web2/src/components/RegisterForm/RegisterForm.tsx
@@ -1,10 +1,5 @@
 import assert from 'assert';
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 import { useHistory, Link } from 'react-router-dom';
 import ReCAPTCHA from 'react-google-recaptcha';
 
@@ -12,7 +7,7 @@ import Recaptcha from '../Recaptcha/Recaptcha';
 import { registerUser, logIn } from '../../lib/auth';
 import { useUserContext } from '../../contexts/UserContext';
 
-import './RegisterForm.css';
+import logo from '../../assets/wya-logo.png';
 
 export default function RegisterForm(): JSX.Element {
   const history = useHistory();
@@ -24,7 +19,7 @@ export default function RegisterForm(): JSX.Element {
     if (user) {
       // eslint-disable-next-line
       console.log('User already logged in, redirecting to home page');
-      history.push('/calendar');
+      history.push('/dashboard');
     }
   });
 
@@ -44,47 +39,134 @@ export default function RegisterForm(): JSX.Element {
 
     await registerUser(formValue);
     await logIn(email, password);
-    history.push('/calendar');
+    history.push('/dashboard');
   };
 
   return (
-    <Container>
-      <Col className="form-container">
-        <Form onSubmit={registerUserHandler} className="form-register">
-          <Row>
-            <Form.Group as={Col} controlId="registerFirstName">
-              <Form.Label style={{ margin: 0 }}>First Name</Form.Label>
-              <Form.Control type="text" name="firstName" />
-            </Form.Group>
+    <>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img className="mx-auto h-12 w-auto" src={logo} alt="wya? logo" />
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Create your new account
+        </h2>
+      </div>
 
-            <Form.Group as={Col} controlId="registerLastName">
-              <Form.Label style={{ margin: 0 }}>Last Name</Form.Label>
-              <Form.Control type="text" name="lastName" />
-            </Form.Group>
-          </Row>
-          <Row>
-            <Form.Group controlId="registerEmail">
-              <Form.Label style={{ margin: 0 }}>Email</Form.Label>
-              <Form.Control type="email" name="email" />
-            </Form.Group>
-          </Row>
-          <Row>
-            <Form.Group controlId="registerPassword">
-              <Form.Label style={{ margin: 0 }}>Password</Form.Label>
-              <Form.Control type="password" name="password" />
-            </Form.Group>
-          </Row>
-          <Button type="submit" className="form-button">
-            Register
-          </Button>
-          <Form.Text>
-            <Link className="clickable-link" to="/login">
-              Have an account?
-            </Link>
-          </Form.Text>
-          <Recaptcha recaptchaRef={recaptchaRef} />
-        </Form>
-      </Col>
-    </Container>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={registerUserHandler}>
+            <div>
+              <label
+                htmlFor="firstName"
+                className="block text-sm font-medium text-gray-700"
+              >
+                First Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  autoComplete="on"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="lastName"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Last Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  autoComplete="on"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <Link
+                  to="/login"
+                  className="font-medium text-indigo-600 hover:text-indigo-500 no-underline"
+                >
+                  Already have an account?
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Register
+              </button>
+            </div>
+            <Recaptcha recaptchaRef={recaptchaRef} />
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
