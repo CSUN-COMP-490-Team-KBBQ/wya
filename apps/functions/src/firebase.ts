e809a5e60563512dfd15194203f01e7a5308c340
Jan Navarro <navarrojanderrick@gmail.com>
2022-04-03 16:00:32 -0700

    Remove apps/functions

--- apps/functions/src/firebase.ts
+++
@@ -1,8 +0,0 @@
-export * as functions from 'firebase-functions';
-import { getAuth as getFirebaseAuth } from 'firebase-admin/auth';
-import { getFirestore as getFirebaseFirestore } from 'firebase-admin/firestore';
-import { makeFirebaseClient } from 'wya-api/src/modules/etl/firebase/make-firebase-client';
-
-export const firebaseClient = makeFirebaseClient();
-export const auth = getFirebaseAuth(firebaseClient);
-export const firestore = getFirebaseFirestore(firebaseClient);
--- apps/functions/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { functions } from './firebase';
-import app from './app';
-
-// Using express and exposing functions as express widget
-// https://firebase.google.com/docs/functions/http-events#using_existing_express_apps
-export const api = functions.https.onRequest(app);
