8cdf811ec2611ff599ba68a89e294056439aeb45
MarMar Tahery <mtahery20@hotmail.com>
2022-04-09 01:19:50 -0700

    Complete homepage design

--- apps/web2/src/pages/LandingPage/LandingPage.tsx
+++ apps/web2/src/pages/LandingPage/LandingPage.tsx
@@ -3,8 +3,8 @@ import { Link, useHistory } from 'react-router-dom';
 
 import { useUserContext } from '../../contexts/UserContext';
 
-import logo from '../../assets/wya-logo.png';
-import image from '../../assets/background9.png';
+// import logo from '../../assets/wya-logo.png';
+import image from '../../assets/background13.png';
 
 export default function LandingPage() {
   const history = useHistory();
@@ -23,26 +23,26 @@ export default function LandingPage() {
           <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 h-screen">
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
               <nav
-                className="relative flex items-center justify-between sm:h-10"
+                className="relative flex items-center justify-between"
                 aria-label="Global"
               >
                 <div className="flex items-center flex-grow flex-shrink-0">
                   <div className="flex items-center justify-between w-full">
                     {/* added for build to pass */}
                     <a href="example.com">
-                      <img
+                      {/* <img
                         className="h-8 w-auto sm:h-10"
                         src={logo}
                         alt="wya? logo"
-                      />
+                      /> */}
                     </a>
                   </div>
-                  <Link
+                  {/* <Link
                     to="/login"
                     className="flex justify-end font-large w-full z-10 text-blue-600 hover:text-blue-500 no-underline"
                   >
                     Log in
-                  </Link>
+                  </Link> */}
                 </div>
               </nav>
             </div>
@@ -50,9 +50,9 @@ export default function LandingPage() {
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block xl:inline">Data to enrich your</span>{' '}
+                  <span className="block xl:inline">Scheduling</span>{' '}
                   <span className="block text-blue-600 xl:inline">
-                    scheduling
+                    Simplified
                   </span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
@@ -62,16 +62,22 @@ export default function LandingPage() {
                   stacking personal schedules so that your group will be able to
                   clearly see who&apos;s doing what and when. The add-a-friend
                   feature and overall simplicity of wya? removes the need for
-                  multiple apps and lets you chat, plan, and invite whomever
-                  you&apos;d like.
+                  multiple apps and lets you add friends, plan, and invite whomever
+                  you&apos;d like!
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                  <div className="rounded-md shadow">
+                  <div className="inline-flex rounded-md shadow">
                     <Link
                       to="/register"
-                      className="no-underline w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+                      className="no-underline flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                     >
                       Get started
+                    </Link>
+                    <Link
+                      to="/login"
+                      className="no-underline flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+                    >
+                    Log In
                     </Link>
                   </div>
                 </div>
