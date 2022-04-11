44d1bec8589651c92f17b4b9db37c7922e487bcd
Jorge Escobar <jorge.escobar0419@gmail.com>
2022-04-10 17:25:38 -0700

    Remove unused useState import from SettingsPage in web2

--- apps/web2/src/pages/SettingsPage/SettingsPage.tsx
+++ apps/web2/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React from 'react';
 import { Link, useLocation, useHistory } from 'react-router-dom';
 
 import { useUserRecordContext } from '../../contexts/UserRecordContext';
