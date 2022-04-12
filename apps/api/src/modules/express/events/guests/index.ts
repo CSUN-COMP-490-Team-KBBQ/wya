fec1f27fd04410beeb62e531852ae55fa68a0ae5
Ivan Cuadra <ivan.cuadra.869@my.csun.edu>
2022-04-12 10:54:56 -0700

    Add event guests update functionality
    For now, the update guests button delete guests that have declined.

--- apps/api/src/modules/express/events/guests/index.ts
+++ apps/api/src/modules/express/events/guests/index.ts
@@ -1,19 +1,39 @@
 import { Router } from 'express';
 
 import { firebaseClient, functions } from '../../../firebase';
+import { etlEventsGuestsUpdateStatus } from '../../../etl/events/guests/updateStatus';
 import { etlEventsGuestsUpdate } from '../../../etl/events/guests/update';
+
 import { etlEventsGuestsDelete } from '../../../etl/events/guests/delete';
 
 const router = Router();
 
-router.post('/update', async (req, res, next) => {
+router.post('/update-status', async (req, res, next) => {
   const logger = functions.logger;
 
   const { status, uid, eventId } = req.body;
 
   try {
-    await etlEventsGuestsUpdate(
+    await etlEventsGuestsUpdateStatus(
       { status, uid, eventId },
+      { firebaseClientInjection: firebaseClient },
+      { debug: logger.info }
+    );
+
+    res.sendStatus(200);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/update', async (req, res, next) => {
+  const logger = functions.logger;
+
+  const { eventId, hostId, eventGuests } = req.body;
+
+  try {
+    await etlEventsGuestsUpdate(
+      { eventId, hostId, eventGuests },
       { firebaseClientInjection: firebaseClient },
       { debug: logger.info }
     );
