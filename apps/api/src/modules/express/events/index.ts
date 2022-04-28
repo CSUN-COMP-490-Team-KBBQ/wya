b355bebd67d4269f54b7fb771d7b3aa30e88b9d5
Ivan Cuadra <ivan.cuadra.869@my.csun.edu>
2022-04-28 10:22:17 -0700

    Add router for update-guests and delete-guests

--- apps/api/src/modules/express/events/index.ts
+++ apps/api/src/modules/express/events/index.ts
@@ -6,6 +6,8 @@ import { functions, firebaseClient } from '../../firebase';
 import { etlEventsCreate } from '../../etl/events/create';
 import { etlEventsDelete } from '../../etl/events/delete';
 import { etlEventsUpdate } from '../../etl/events/update';
+import { etlEventsDeleteGuests } from '../../etl/events/delete-guests';
+import { etlEventsUpdateGuests } from '../../etl/events/update-guests';
 
 const router = Router();
 
@@ -47,6 +49,38 @@ router.post('/update', async (req, res, next) => {
     const context = await authenticate(req);
 
     const { data, errors } = await etlEventsUpdate(params, context, {
+      debug: functions.logger.info,
+      firebaseClientInjection: firebaseClient,
+    });
+
+    return res.status(200).json({ data, errors });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post('/delete-guests', async (req, res, next) => {
+  try {
+    const params = req.body;
+    const context = await authenticate(req);
+
+    const { data } = await etlEventsDeleteGuests(params, context, {
+      debug: functions.logger.info,
+      firebaseClientInjection: firebaseClient,
+    });
+
+    return res.status(200).json({ data });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post('/update-guests', async (req, res, next) => {
+  try {
+    const params = req.body;
+    const context = await authenticate(req);
+
+    const { data, errors } = await etlEventsUpdateGuests(params, context, {
       debug: functions.logger.info,
       firebaseClientInjection: firebaseClient,
     });
