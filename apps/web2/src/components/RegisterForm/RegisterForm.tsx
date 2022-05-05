817d3b929db1c8be3fd9cba93cc1b4d8d1223e00
MarMar Tahery <85257802+mtahery20@users.noreply.github.com>
2022-05-05 10:09:15 -0700

    Register Form changes

--- apps/web2/src/components/RegisterForm/RegisterForm.tsx
+++ apps/web2/src/components/RegisterForm/RegisterForm.tsx
@@ -8,20 +8,21 @@ import { registerUser, logIn } from '../../lib/auth';
 import { useUserContext } from '../../contexts/UserContext';
 
 import logo from '../../assets/wya-logo.png';
+import { XCircleIcon } from '@heroicons/react/solid';
 
 export default function RegisterForm(): JSX.Element {
   const history = useHistory();
   const { user } = useUserContext();
+  const [displayError, setDisplayError] = React.useState<string>('');
 
   const recaptchaRef = React.useRef<ReCAPTCHA>(null);
 
   React.useEffect(() => {
-    if (user) {
-      // eslint-disable-next-line
-      console.log('User already logged in, redirecting to home page');
-      history.push('/dashboard');
-    }
-  });
+    // if (user) {
+    //   // eslint-disable-next-line
+    //   history.push('/dashboard');
+    // }
+  }, [displayError]);
 
   const registerUserHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -30,16 +31,26 @@ export default function RegisterForm(): JSX.Element {
       formData.entries()
       // eslint-disable-next-line
     ) as any;
-    const { email, password } = formValue;
+    const { email, password, confirmPassword } = formValue;
 
     assert(recaptchaRef.current, 'ReCAPTCHA has not loaded');
     const token = await recaptchaRef.current.executeAsync();
 
     assert(token, 'Missing ReCAPTCHA token');
 
-    await registerUser(formValue);
-    await logIn(email, password);
-    history.push('/dashboard');
+    if (password !== confirmPassword) {
+      setDisplayError('Passwords do not match!');
+    } else {
+      // eslint-disable-next-line
+      await registerUser(formValue)
+        .then(async () => {
+          await logIn(email, password);
+          history.push('/dashboard');
+        })
+        .catch((err) => {
+          setDisplayError('Email address is already in use!');
+        });
+    }
   };
 
   return (
@@ -50,6 +61,25 @@ export default function RegisterForm(): JSX.Element {
           Create your new account
         </h2>
       </div>
+      {/* Negative Alert Banner */}
+      {displayError.length > 0 && (
+        <div className="rounded-md mb-4 bg-red-50 p-4 sm:mx-auto">
+          <div className="flex">
+            <div className="flex-shrink-0">
+              <XCircleIcon
+                className="h-5 w-5 text-red-400"
+                aria-hidden="true"
+              />
+            </div>
+            <div className="ml-3">
+              <h3 className="text-sm font-medium text-red-800">
+                {displayError}
+              </h3>
+              <div className="mt-2 text-sm text-red-700">Please try again</div>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
@@ -123,6 +153,24 @@ export default function RegisterForm(): JSX.Element {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Comfirm Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
