4e9fb2fda0f5e777ee5d1ba7106ea09e048fab22
Amelia Aboujawdah <amelia.abou6363@gmail.com>
2022-05-09 01:44:11 -0700

    initial redesign changes

--- apps/web/src/pages/EventPage/EventPage.tsx
+++ apps/web/src/pages/EventPage/EventPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 import { useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 
 import {
   EventDocument,
@@ -27,6 +28,7 @@ export default function EventPage({
     };
   };
 }): JSX.Element {
+  const history = useHistory();
   const { userRecord } = useUserRecordContext();
   const [eventData, setEventData] = useState<EventDocument>();
   const [eventGuests, setEventGuests] = useState<EventGuest[]>([]);
@@ -150,47 +152,98 @@ export default function EventPage({
   if (userRecord && eventData && eventGuests) {
     return (
       <Sidebar>
-        <div id="eventFinalizedContent">
-          <h1>{eventData.name}</h1>
-          <div id="eventFinalizedPs">
-            <p>
-              <u>Description</u>: {eventData.description}
-            </p>
-            {/* add who is hosting here? */}
-            <p>
-              <u>Day</u>: {eventData.day}
-            </p>
-            <p>
-              <u>Starts</u>: {eventData.dailyStartTime}
-            </p>
-            <p>
-              <u>Ends</u>: {eventData.dailyEndTime}
-            </p>
+        <div
+          id="eventContainer"
+          className="text-left bg-white shadow overflow-hidden sm:rounded-lg"
+        >
+          <div className="mb-2 px-4 pt-5 sm:px-6">
+            <h3 className="text-left text-lg leading-6 font-medium text-gray-900">
+              {eventData.name}
+            </h3>
           </div>
-          {/* render for a guest only */}
-          {eventData.hostId !== userRecord.uid && (
-            <div id="eventFinalizedButtons">
-              <Button onClick={handleDecline} variant="danger">
-                Decline
-              </Button>
-              <Button onClick={handleAccept} variant="success">
-                Accept
-              </Button>
-            </div>
-          )}
-          <div>
-            <h2>Attending</h2>
-            <ListGroup className="attending-list">
-              {eventGuests.map((guestName) => {
-                return guestName.status === 'ACCEPTED' ? (
-                  <ListGroup.Item>
-                    {guestName.firstName} {guestName.lastName}
-                  </ListGroup.Item>
-                ) : (
-                  <></>
-                );
-              })}
-            </ListGroup>
+          <div className="">
+            <dl className="sm:divide-y sm:divide-gray-200">
+              <div className="sm:col-span-1">
+                <dt className="text-sm font-medium text-gray-500">Full name</dt>
+                <dd className="mt-1 text-sm text-gray-900">Margot Foster</dd>
+              </div>
+              <div className="sm:col-span-1">
+                <dt className="text-sm font-medium text-gray-500">
+                  Application for
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900">
+                  Backend Developer
+                </dd>
+              </div>
+              <div className="py-4 mt-6 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-left text-sm font-medium text-gray-500">
+                  Description
+                </dt>
+                <dd className="text-left mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {eventData.description}
+                </dd>
+              </div>
+              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-left text-sm font-medium text-gray-500">
+                  Date
+                </dt>
+                <dd className="text-left mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {eventData.day}
+                </dd>
+              </div>
+              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-left text-sm font-medium text-gray-500">
+                  Time
+                </dt>
+                <dd className="text-left mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {eventData.dailyStartTime} to {eventData.dailyEndTime}
+                </dd>
+              </div>
+
+              {/* render for a guest only */}
+              {eventData.hostId !== userRecord.uid && (
+                <div id="eventFinalizedButtons">
+                  <Button onClick={handleDecline} variant="danger">
+                    Decline
+                  </Button>
+                  <Button onClick={handleAccept} variant="success">
+                    Accept
+                  </Button>
+                </div>
+              )}
+              <div className="py-4 mt-6 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-left text-sm font-medium text-gray-500">
+                  Attending
+                </dt>
+                <dd className="text-left mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  <ListGroup className="attending-list">
+                    {eventGuests.map((guestName) => {
+                      return guestName.status === 'ACCEPTED' ? (
+                        <ListGroup.Item>
+                          {guestName.firstName} {guestName.lastName}
+                        </ListGroup.Item>
+                      ) : (
+                        <></>
+                      );
+                    })}
+                  </ListGroup>
+                </dd>
+              </div>
+              <div>
+                {/* <h2>Attending</h2>
+                <ListGroup className="attending-list">
+                  {eventGuests.map((guestName) => {
+                    return guestName.status === 'ACCEPTED' ? (
+                      <ListGroup.Item>
+                        {guestName.firstName} {guestName.lastName}
+                      </ListGroup.Item>
+                    ) : (
+                      <></>
+                    );
+                  })}
+                </ListGroup> */}
+              </div>
+            </dl>
           </div>
         </div>
       </Sidebar>
@@ -199,8 +252,8 @@ export default function EventPage({
 
   // default render
   return (
-    <Page>
-      <></>
-    </Page>
+    // <Page>
+    <></>
+    // {/* </Page> */}
   );
 }
