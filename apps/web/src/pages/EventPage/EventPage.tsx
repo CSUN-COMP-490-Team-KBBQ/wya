9937ffabe19f78e74fe8d50782b1a8382adbc5db
Amelia Aboujawdah <amelia.abou6363@gmail.com>
2022-05-09 03:04:36 -0700

    KSP-204 add buttons for host/attendee event options, finish redesign

--- apps/web/src/pages/EventPage/EventPage.tsx
+++ apps/web/src/pages/EventPage/EventPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 import { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
+import { MinusSmIcon as MinusIconSolid } from '@heroicons/react/solid';
 
 import {
   EventDocument,
@@ -111,48 +112,167 @@ export default function EventPage({
 
   // The following code is here to keep a record of how to use the api calls
 
-  // const handleDelete = async () => {
-  //   if (eventData !== undefined && userRecord) {
-  //     await api.post(
-  //       '/events/delete',
-  //       JSON.stringify({ eventId: eventData.eventId })
-  //     );
+  const handleDelete = async () => {
+    if (eventData !== undefined && userRecord) {
+      await api.post(
+        '/events/delete',
+        JSON.stringify({ eventId: eventData.eventId })
+      );
 
-  //     console.log('Event deleted');
-  //     history.push('/calendar');
-  //   }
-  // };
+      console.log('Event deleted');
+      history.push('/calendar');
+    }
+  };
 
-  // const handleRemove = async () => {
-  //   if (eventData !== undefined && userRecord) {
-  //     await api.post('/events/guests/delete', { eventId: eventData.eventId });
+  const handleRemove = async () => {
+    if (eventData !== undefined && userRecord) {
+      await api.post('/events/guests/delete', { eventId: eventData.eventId });
 
-  //     console.log('Event Guest removed');
-  //     history.push('/calendar');
-  //   }
-  // };
+      console.log('Event Guest removed');
+      history.push('/calendar');
+    }
+  };
 
-  // const handleUpdateGuests = async () => {
-  //   if (eventData !== undefined && userRecord) {
-  //     await api.post('events/update-guests', {
-  //       eventId: eventData.eventId,
-  //       guestsByUserId: eventGuests
-  //         .filter(
-  //           (guest) =>
-  //             guest.status === EVENT_GUEST_STATUS.ACCEPTED ||
-  //             guest.status === EVENT_GUEST_STATUS.PENDING
-  //         )
-  //         .map((guest) => guest.uid),
-  //     });
+  const handleUpdateGuests = async () => {
+    if (eventData !== undefined && userRecord) {
+      await api.post('events/update-guests', {
+        eventId: eventData.eventId,
+        guestsByUserId: eventGuests
+          .filter(
+            (guest) =>
+              guest.status === EVENT_GUEST_STATUS.ACCEPTED ||
+              guest.status === EVENT_GUEST_STATUS.PENDING
+          )
+          .map((guest) => guest.uid),
+      });
 
-  //     console.log('Guests Updated');
-  //   }
-  // };
+      console.log('Guests Updated');
+    }
+  };
 
   if (userRecord && eventData && eventGuests) {
     return (
       <Sidebar>
-        <div
+        <div className="mx-auto w-8/12 h-screen bg-white shadow overflow-auto sm:rounded-lg">
+          <div className="px-4 py-5 sm:px-6">
+            <h1 className="flex justify-center">{eventData.name}</h1>
+          </div>
+          <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+            <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+              <div className="sm:col-span-1">
+                <dt className="text-sm font-medium text-gray-500">Date</dt>
+                <dd className="mt-1 text-sm text-gray-900">{eventData.day}</dd>
+              </div>
+              <div className="sm:col-span-1">
+                <dt className="text-sm font-medium text-gray-500">Time</dt>
+                <dd className="mt-1 text-sm text-gray-900">
+                  {eventData.dailyStartTime} - {eventData.dailyEndTime}
+                </dd>
+              </div>
+              <div className="sm:col-span-2">
+                <dt className="text-sm font-medium text-gray-500">
+                  Description
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900">
+                  {eventData.description}
+                </dd>
+              </div>
+              {/* render for a guest only */}
+              {eventData.hostId !== userRecord.uid && (
+                <div className="sm:col-span-2">
+                  <dd className="mt-1 text-sm text-gray-900">
+                    <div id="eventFinalizedButtons">
+                      <button
+                        type="button"
+                        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        onClick={handleAccept}
+                      >
+                        Accept
+                      </button>
+                      <button
+                        type="button"
+                        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                        onClick={handleDecline}
+                      >
+                        Decline
+                      </button>
+                      <button
+                        type="button"
+                        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-500 hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
+                        onClick={handleRemove}
+                      >
+                        Leave
+                      </button>
+                    </div>
+                  </dd>
+                </div>
+              )}
+              {/* render for the host only */}
+              {eventData.hostId === userRecord.uid && (
+                <div className="sm:col-span-2">
+                  <dd className="mt-1 text-sm text-gray-900">
+                    <div id="eventFinalizedButtons">
+                      <button
+                        type="button"
+                        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        onClick={handleUpdateGuests}
+                      >
+                        Update Guest List
+                      </button>
+                      <button
+                        type="button"
+                        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                        onClick={handleDelete}
+                      >
+                        Delete Event
+                      </button>
+                    </div>
+                  </dd>
+                </div>
+              )}
+              <div className="sm:col-span-2">
+                <dt className="text-sm font-medium text-gray-500">Attendees</dt>
+                <dd className="mt-1 text-sm text-gray-900">
+                  <ul
+                    role="list"
+                    className="max-w-sm border border-gray-200 rounded-md divide-y divide-gray-200"
+                  >
+                    {eventGuests.map((guestName) => {
+                      return guestName.status === 'ACCEPTED' ? (
+                        <li className="pr-4 py-3 flex items-center justify-between text-sm">
+                          <div className="w-0 flex-1 flex items-center">
+                            <span className="ml-2 flex-1 w-0 truncate">
+                              {guestName.firstName} {guestName.lastName}
+                            </span>
+                          </div>
+                          {/* render for the host only */}
+                          {/* TODO: allow host to remove guests */}
+                          {/* {eventData.hostId === userRecord.uid && (
+                            <dd className="mt-1 text-sm text-gray-900">
+                              <button
+                                type="button"
+                                className="inline-flex items-end p-1 border border-transparent rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                                onClick={handleRemove}
+                              >
+                                <MinusIconSolid
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
+                              </button>
+                            </dd>
+                          )} */}
+                        </li>
+                      ) : (
+                        <></>
+                      );
+                    })}
+                  </ul>
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+        {/* <div
           id="eventContainer"
           className="text-left bg-white shadow overflow-hidden sm:rounded-lg"
         >
@@ -198,10 +318,10 @@ export default function EventPage({
                 <dd className="text-left mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                   {eventData.dailyStartTime} to {eventData.dailyEndTime}
                 </dd>
-              </div>
+              </div> */}
 
-              {/* render for a guest only */}
-              {eventData.hostId !== userRecord.uid && (
+        {/* render for a guest only */}
+        {/* {eventData.hostId !== userRecord.uid && (
                 <div id="eventFinalizedButtons">
                   <Button onClick={handleDecline} variant="danger">
                     Decline
@@ -229,23 +349,10 @@ export default function EventPage({
                   </ListGroup>
                 </dd>
               </div>
-              <div>
-                {/* <h2>Attending</h2>
-                <ListGroup className="attending-list">
-                  {eventGuests.map((guestName) => {
-                    return guestName.status === 'ACCEPTED' ? (
-                      <ListGroup.Item>
-                        {guestName.firstName} {guestName.lastName}
-                      </ListGroup.Item>
-                    ) : (
-                      <></>
-                    );
-                  })}
-                </ListGroup> */}
-              </div>
+              <div></div>
             </dl>
           </div>
-        </div>
+        </div> */}
       </Sidebar>
     );
   }
