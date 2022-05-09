17607bf424e65c32290e4b1c0adec1d6f1514d9f
Amelia Aboujawdah <amelia.abou6363@gmail.com>
2022-05-09 11:32:49 -0700

    KSP-204 remove handleUpdateGuests function and corresponding button

--- apps/web/src/pages/EventPage/EventPage.tsx
+++ apps/web/src/pages/EventPage/EventPage.tsx
@@ -130,23 +130,6 @@ export default function EventPage({
     }
   };
 
-  const handleUpdateGuests = async () => {
-    if (eventData !== undefined && userRecord) {
-      await api.post('events/update-guests', {
-        eventId: eventData.eventId,
-        guestsByUserId: eventGuests
-          .filter(
-            (guest) =>
-              guest.status === EVENT_GUEST_STATUS.ACCEPTED ||
-              guest.status === EVENT_GUEST_STATUS.PENDING
-          )
-          .map((guest) => guest.uid),
-      });
-
-      console.log('Guests Updated');
-    }
-  };
-
   if (userRecord && eventData && eventGuests) {
     return (
       <Sidebar>
@@ -209,13 +192,6 @@ export default function EventPage({
                 <div className="sm:col-span-2">
                   <dd className="mt-1 text-sm text-gray-900">
                     <div id="eventFinalizedButtons">
-                      <button
-                        type="button"
-                        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                        onClick={handleUpdateGuests}
-                      >
-                        Update Guest List
-                      </button>
                       <button
                         type="button"
                         className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
